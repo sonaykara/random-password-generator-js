a846a7d484ac52c715b4ee4ecfb7beba7dd5d6e3
zonayovic <108528598+zonayovic@users.noreply.github.com>
2024-02-13 01:46:05 +0300

    NUmber Of Characters

--- project/script.js
+++ project/script.js
@@ -1,7 +1,8 @@
 const btnGenerate = document.querySelector(".btn-generate");
-const input = document.getElementById("pass")
+const input = document.getElementById("password")
 const copy = document.getElementById("copy")
 const alertPass = document.getElementById("alert-pass")
+const passwordCharacters = document.getElementById("password-characters")
 
 
 
@@ -9,24 +10,26 @@ btnGenerate.addEventListener("click", createPassword );
 copy.addEventListener("click" , copyPass)
 
 function createPassword() {
-    const passworLength = 14;
+
+    const passwordLength = parseInt(passwordCharacters.value);
     const char = '0123456789abcedfghijklmnoprstuvyz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let password = ""; 
 
-    for (let index = 0; index < passworLength; index++) {
+    for (let index = 0; index < passwordLength; index++) {
         const randomNumber = Math.floor(Math.random()* char.length)
         password += char[randomNumber]
         
     }
     
     input.value = password
-    
+    passwordCharacters.value = ""
 }
 
 function copyPass () {
    if(input.value) {
     input.select()
     navigator.clipboard.writeText(input.value)
+    input.value = ""
     alertPass.classList.remove("active")
     setTimeout(() => {
         alertPass.classList.add("active")
